1b532e7ffb172e2cb6094d39e2e1ba8a47f79c47
Alan Santiago <alanivan.s06@gmail.com>
2019-08-11 15:05:59 -0500

    Add build/ for deployment

---
+++ build/precache-manifest.00d7a9d5c05d7fa0e1e65519b8276f8d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce899d7d213bfd0c403bbee24f370282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f2ee99e03c34bdc9ef",
+    "url": "/static/css/main.a2945816.chunk.css"
+  },
+  {
+    "revision": "813c01f422f2c636965f",
+    "url": "/static/js/2.3d3cee33.chunk.js"
+  },
+  {
+    "revision": "c8f2ee99e03c34bdc9ef",
+    "url": "/static/js/main.d2f25a23.chunk.js"
+  },
+  {
+    "revision": "4e6befa2d3fe2c7dcd00",
+    "url": "/static/js/runtime~main.fdeef067.js"
+  },
+  {
+    "revision": "fd184cfc1ef1f4e6d1d63b35971c6b4c",
+    "url": "/static/media/background.fd184cfc.png"
+  },
+  {
+    "revision": "92236ceae3565614eb8b7f3f9839a90f",
+    "url": "/static/media/background2x.92236cea.png"
+  },
+  {
+    "revision": "ba2e415e817351fb402d07afd2be5ceb",
+    "url": "/static/media/easter-a.ba2e415e.png"
+  },
+  {
+    "revision": "7d7a81dcaa54ebefe86d47f90c6bb589",
+    "url": "/static/media/easter-b.7d7a81dc.png"
+  },
+  {
+    "revision": "25c6f861860963cb105716cc999b5174",
+    "url": "/static/media/invie-acustic.25c6f861.png"
+  },
+  {
+    "revision": "e039fff97126b1b18d68f4be45bf6d25",
+    "url": "/static/media/invie-classic.e039fff9.png"
+  },
+  {
+    "revision": "45598562077bdc84281b5330ff4d283e",
+    "url": "/static/media/like.45598562.png"
+  },
+  {
+    "revision": "70297162b2ac4eb6ac54a0ddd5f85bfe",
+    "url": "/static/media/team-platzi.70297162.jpg"
+  }
+]);
